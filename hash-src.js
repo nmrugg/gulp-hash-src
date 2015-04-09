2a6334113892407759cf094ce0aa0ec7532717db
Nathan Rugg <nmrugg@gmail.com>
2015-04-10 07:25:09 +0800

    Added more default exts.

--- hash-src.js
+++ hash-src.js
@@ -85,7 +85,7 @@ module.exports = function hash_src(options)
         options.enc = "hex";
     }
     if (!options.exts) {
-        options.exts = [".js", ".css", ".jpg", ".jpeg", ".png", ".gif", ".webp", ".svg", ".pdf", ".ico", ".ttf", ".woff", ".mp3", ".ogg", ".ogv", ".mp4", ".webm"];
+        options.exts = [".js", ".css", ".jpg", ".jpeg", ".png", ".gif", ".webp", ".svg", ".pdf", ".ico", ".ttf", ".woff", ".mp3", ".ogg", ".ogv", ".mp4", ".webm", ".zip", ".tar", ".gz", ".bz2"];
     }
     if (!options.regex) {
         options.regex = find_regex;
